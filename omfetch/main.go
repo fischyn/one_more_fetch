b915ab02f914297e7a9e1e82c843a6bb726909d4
Maksym Fishchyn <max.freedman.main@gmail.com>
2025-05-18 21:24:01 +0300

    Just for fun - a litle bit of refactoring main.go file mess

--- omfetch/main.go
+++ omfetch/main.go
@@ -4,46 +4,13 @@ import (
 	"context"
 	"fmt"
 
+	"github/com/fischyn/omfetch/img/ascii"
 	"github/com/fischyn/omfetch/sys/bios"
 	"github/com/fischyn/omfetch/sys/cpu"
 	"github/com/fischyn/omfetch/sys/gpu"
 	"github/com/fischyn/omfetch/sys/host"
 	"github/com/fischyn/omfetch/sys/mem"
 )
-
-const Logo = `
-           ▄▄▄▄▄
-        ▄█████████▄
-      ▄███▀▀▀▀▀▀▀███▄
-     ███▀          ▀███
-    ███              ███
-    ███              ███
-     ███▄          ▄███
-      ▀███▄▄▄▄▄▄▄███▀
-         ▀▀▀▀▀▀▀▀▀
-`
-
-const Logo2 = "       .------..\n" +
-	"     -          -\n" +
-	"   /              \\\n" +
-	" /                   \\\n" +
-	"/    .--._    .---.   |\n" +
-	"|  /      -__-     \\   |\n" +
-	"| |                 |  |\n" +
-	"||     ._   _.      ||\n" +
-	"||      o   o       ||\n" +
-	"||      _  |_      ||\n" +
-	"C|     (o\\_/o)     |O     Uhhh, this computer\n" +
-	" \\      _____      /       is like, busted or\n" +
-	"   \\ ( /#####\\ ) /       something. So go away.\n" +
-	"    \\  `====='  /\n" +
-	"     \\  -___-  /\n" +
-	"      |       |\n" +
-	"      /-_____-\\" + "\n" +
-	"    /           \\\n" +
-	"  /               \\\n" +
-	" /__|  AC / DC  |__\\\n" +
-	" | ||           |\\ \\"
 
 var (
 	blue  = "\033[34m"
@@ -55,29 +22,59 @@ func PrintInfo(label string, value string) {
 	fmt.Printf("%s%-12s%s %s\n", green, label+":", reset, value)
 }
 
+func formatMB(bytes uint64) string {
+	return fmt.Sprintf("%d MB", bytes/1024/1024)
+}
+
+func formatCPU(model string, cores int32, mhz float64) string {
+	return fmt.Sprintf("%s (%d cores) %.2f MHz", model, cores, mhz)
+}
+
+func printPlatformInfo(ctx context.Context) {
+	platform, family, version, displayVersion, err := host.GetPlatformInfo(ctx)
+	if err == nil {
+		PrintInfo("OS", fmt.Sprintf("%s %s %s %s", platform, family, version, displayVersion))
+	}
+}
+
+func printCPUInfo(ctx context.Context) {
+	cpuInfo, err := cpu.GetCPUInfo(ctx)
+	if err == nil && len(cpuInfo) > 0 {
+		c := cpuInfo[0]
+		PrintInfo("CPU", formatCPU(c.ModelName, c.Cores, c.Mhz))
+	}
+}
+
+func printMemoryInfo(ctx context.Context) {
+	memInfo, err := mem.GetMemoryInfo(ctx)
+	if err == nil {
+		PrintInfo("RAM", formatMB(memInfo.Total))
+		PrintInfo("RAM(Used)", formatMB(memInfo.Used))
+	}
+}
+
+func printGPUInfo(ctx context.Context) {
+	gpuInfo, err := gpu.GetGPUInfo(ctx)
+	if err == nil && len(gpuInfo) > 0 {
+		PrintInfo("GPU", gpuInfo[0].GPUName)
+	}
+}
+
+func printBiosInfo(ctx context.Context) {
+	product, manufacturer, family, version, err := bios.GetBiosInfo(ctx)
+	if err == nil {
+		PrintInfo("BIOS", fmt.Sprintf("%s %s %s %s", manufacturer, family, product, version))
+	}
+}
+
 func main() {
-	//test only lol
+	fmt.Println(blue + ascii.Blinky + reset)
 
-	fmt.Println(blue + Logo2 + reset)
+	ctx := context.Background()
 
-	platform, family, platformVersion, displayVersion, _ := host.GetPlatformInfo(context.Background())
-	PrintInfo("OS", platform+" "+family+" "+platformVersion+" "+displayVersion)
-
-	cpuInfo, _ := cpu.GetCPUInfo(context.Background())
-	c1 := cpuInfo[0]
-
-	PrintInfo("CPU", fmt.Sprintf("%s (%d) cores %f Mhz", c1.ModelName, c1.Cores, c1.Mhz))
-
-	memoryInfo, _ := mem.GetMemoryInfo(context.Background())
-
-	PrintInfo("RAM", fmt.Sprintf("%d MB", memoryInfo.Total/1024/1024))
-	PrintInfo("RAM(Used)", fmt.Sprintf("%d MB", memoryInfo.Used/1024/1024))
-
-	gpuInfo, _ := gpu.GetGPUInfo(context.Background())
-
-	PrintInfo("GPU", gpuInfo[0].GPUName)
-
-	product, manufacturer, family, biosVersion, _ := bios.GetBiosInfo(context.Background())
-
-	PrintInfo("BIOS", fmt.Sprintf("%s %s %s %s", manufacturer, family, product, biosVersion))
+	printPlatformInfo(ctx)
+	printCPUInfo(ctx)
+	printMemoryInfo(ctx)
+	printGPUInfo(ctx)
+	printBiosInfo(ctx)
 }
